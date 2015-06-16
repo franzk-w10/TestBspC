61dc6adec9fe6d18dada6f6bcb57696344ab9ad7
franzk-w10 <franz.kranzler@htlwien10.at>
2015-06-16 19:02:51 +0200

    Version 1 - Überprüfung fehlt

--- TestBspC/Prog_C.java
+++ TestBspC/Prog_C.java
@@ -1,10 +1,23 @@
+import java.util.Random;
+
 import AlgoTools.IO;
 public class Prog_C {
 
 	public static void main(String[] args) {
+		
+		IO.println("\n\tHerzlich Willkommen!\n\n");
+		
+		tipp();
 
-		IO.println("\n\tHerzlich Willkommen!");
-
+	}
+	
+	static void tipp() {
+		Random zufall=new Random();
+		int temp;
+		for (int i=0; i<=5; i++) {
+			temp=zufall.nextInt(45)+1;
+			IO.print(temp,3);
+		}
 	}
 
 }
